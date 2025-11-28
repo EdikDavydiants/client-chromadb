81dfdf2b2c39b8d32f4d1b8e337275e509e0c776
Edik <davydiantsedik@mail.ru>
2025-11-28 14:26:50 +0300

    group changed

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     `maven-publish`
 }
 
-group = "io.github.edikdavydiants"
+group = "com.github.edikdavydiants"
 version = "0.0.1"
 description = "client-chromadb"
 
@@ -27,23 +27,22 @@ dependencyManagement {
 }
 
 dependencies {
-    compileOnly("org.springframework.boot:spring-boot-starter")
+    compileOnly("org.springframework.boot:spring-boot-starter:3.4.0")
 
-    api("com.fasterxml.jackson.core:jackson-core")
-    api("com.fasterxml.jackson.core:jackson-annotations")
-    api("com.fasterxml.jackson.core:jackson-databind")
+    api("com.fasterxml.jackson.core:jackson-core:2.17.2")
+    api("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
+    api("com.fasterxml.jackson.core:jackson-databind:2.17.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.10.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.0")
 
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 
-    implementation("org.yaml:snakeyaml")
+    implementation("org.yaml:snakeyaml:2.2")
 
-    compileOnly("org.springframework.boot:spring-boot-configuration-processor")
-    // annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    compileOnly("org.springframework.boot:spring-boot-configuration-processor:3.4.0")
 }
 
 tasks.withType<Test> {
@@ -54,6 +53,9 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+            groupId = project.group.toString()
+            artifactId = "client-chromadb"
+            version = project.version.toString()
         }
     }
 }
